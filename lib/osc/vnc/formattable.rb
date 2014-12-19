54e09698c41b3f488958b592f30fd53998340e79
Jeremy Nicklas <jnicklas@osc.edu>
2014-12-19 10:12:25 -0500

    added jnlp format

--- lib/osc/vnc/formattable.rb
+++ lib/osc/vnc/formattable.rb
@@ -8,5 +8,33 @@ module OSC
         Display: #{display}
       EOF
     end
+
+    def to_jnlp()
+      <<-EOF.gsub /^\s{8}/, ''
+        <?xml version="1.0" encoding="UTF-8"?>
+        <jnlp spec="1.0+" codebase="http://mirror.osc.edu/mirror" >
+            <information>
+                <title>TightVnc Viewer</title>
+                <vendor>GlavSoft LLC.</vendor>
+                <offline-allowed/>
+            </information>
+            <security>
+                <all-permissions/>
+            </security>
+            <resources>
+                <j2se version="1.6+"/>
+                <jar href="TightVncViewerSigned.jar" main="true"/>
+            </resources>
+            <application-desc main-class="com.tightvnc.vncviewer.VncViewer">
+                <argument>HOST</argument>
+                <argument>#{host}</argument>
+                <argument>PORT</argument>
+                <argument>#{port}</argument>
+                <argument>PASSWORD</argument>
+                <argument>#{password}</argument>
+            </application-desc>
+        </jnlp>
+      EOF
+    end
   end
 end
