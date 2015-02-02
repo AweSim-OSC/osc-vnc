596016def75a5bbffbf68c2f5600d8f062b3c5db
Jeremy Nicklas <jnicklas@osc.edu>
2015-02-02 16:19:21 -0500

    randomly choose a port between 40000-50000

--- lib/osc/vnc/listenable.rb
+++ lib/osc/vnc/listenable.rb
@@ -4,13 +4,26 @@ module OSC::VNC::Listenable
 
   def create_listen_server
     # Listen on random port
-    @listen_server = TCPServer.new(0)
-    @listen_port = @listen_server.addr[1]
+    @listen_port = get_port
+    begin
+      puts @listen_port
+      @listen_server = TCPServer.new(@listen_port)
+    rescue Errno::EADDRINUSE
+      @listen_port = get_port
+      retry
+    end
   end
 
   def read_from_listen_server
     client = listen_server.accept   # wait for connection
     client.read                     # read complete response
   end
+
+  private
+
+    def get_port
+      # Get random number form 40,000 to 50,000
+      rand(40000..50000)
+    end
 
 end
